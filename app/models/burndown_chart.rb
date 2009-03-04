83effcef4237c0d7e392f42da82cb21156ebb171
ciaranj <ciaranj@gmail.com>
2009-03-04 21:33:45 +0000

    Modified the burndown generation algorithm to do more caching.  There is still quite a bit more that could be improved, but its a start. Takes it down from 75 seconds to 6 seconds for rendering one of my sprints (only 85 issues in it!!)
    I'm also not convinced the previous code is rendering the right graph, but this checking is equivalent to the
    previous code (I believe!)

--- app/models/burndown_chart.rb
+++ app/models/burndown_chart.rb
@@ -1,6 +1,15 @@
 class BurndownChart
   attr_accessor :dates, :version, :start_date
   
+  class CachedIssue
+      attr_accessor :issue, :ratio_details_map
+      def initialize(issue)
+        self.issue= issue
+        self.ratio_details_map= issue.journals.map(&:details).flatten.select {|detail| 'done_ratio' == detail.prop_key }
+        self.ratio_details_map.sort! {|a,b| a.journal.created_on <=> b.journal.created_on }
+      end
+  end
+  
   def initialize(version)
     self.version = version
     
@@ -14,23 +23,35 @@ class BurndownChart
   end
   
   def sprint_data
+    cached_all_issues= all_issues.collect do |issue|
+         CachedIssue.new( issue )
+    end
+    
     @sprint_data ||= @dates.map do |date|
-      issues = all_issues.select {|issue| issue.created_on.to_date <= date }
-      issues.inject(0) do |total_hours_left, issue|
-        done_ratio_details = issue.journals.map(&:details).flatten.select {|detail| 'done_ratio' == detail.prop_key }
-        details_today_or_earlier = done_ratio_details.select {|a| a.journal.created_on.to_date <= date }
+      issues = []
+      cached_all_issues.each do |cached_issue| 
+        break if cached_issue.issue.created_on.to_date > date 
+        issues<< cached_issue if cached_issue.issue.created_on.to_date <= date
+      end
 
-        last_done_ratio_change = details_today_or_earlier.sort_by {|a| a.journal.created_on }.last
+      issues.inject(0) do |total_hours_left, cached_issue|
+        details_today_or_earlier = []
+        cached_issue.ratio_details_map.each do |a| 
+            break if a.journal.created_on.to_date > date
+            details_today_or_earlier<< a if a.journal.created_on.to_date <= date
+        end
+
+        last_done_ratio_change = details_today_or_earlier.last
 
         ratio = if last_done_ratio_change
           last_done_ratio_change.value
-        elsif done_ratio_details.size > 0
+        elsif cached_issue.ratio_details_map.size > 0
           0
         else
-          issue.done_ratio.to_i
+          cached_issue.issue.done_ratio.to_i
         end
         
-        total_hours_left += (issue.estimated_hours.to_i * (100-ratio.to_i)/100)
+        total_hours_left += (cached_issue.issue.estimated_hours.to_i * (100-ratio.to_i)/100)
       end
     end
   end
@@ -40,6 +61,7 @@ class BurndownChart
   end
   
   def all_issues
-    version.fixed_issues.find(:all, :include => [{:journals => :details}, :relations_from, :relations_to])
+    issues= version.fixed_issues.find(:all, :include => [{:journals => :details}, :relations_from, :relations_to])
+    issues.sort! {|a,b| a.created_on <=> b.created_on }
   end
 end
