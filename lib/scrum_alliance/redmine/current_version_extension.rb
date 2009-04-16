6fe541474f2e764e99b1ea557154cac914678c75
ciaranj <ciaranj@gmail.com>
2009-04-16 21:42:47 +0100

    Modified the 'current version' logic to select the version who's effective date is closest to 'now' rather than furthest (but still after now)
    When planning sprints in advance this is the most natural 'current version', but obviously
    this breaks down when planning con-current sprints/versions

--- lib/scrum_alliance/redmine/current_version_extension.rb
+++ lib/scrum_alliance/redmine/current_version_extension.rb
@@ -2,7 +2,8 @@ module ScrumAlliance
   module Redmine
     module CurrentVersionExtension
       def current_version
-        versions.detect {|version| version.created_on.to_date <= Date.current && version.effective_date >= Date.current }
+        my_versions= versions.sort {|a,b| a.effective_date <=> b.effective_date }
+        my_versions.detect {|version| version.created_on.to_date <= Date.current && version.effective_date >= Date.current }
       end
     end # CurrentVersionExtension
   end # Redmine
