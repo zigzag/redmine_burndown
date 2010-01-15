ff35a3766e25d53155c72b38b77f7a71a953114a
zigzag <zigzag.chen@gmail.com>
2010-01-15 11:40:12 +0800

    Added Hint to Time Entry page

--- lib/burndown_listener.rb
+++ lib/burndown_listener.rb
@@ -9,12 +9,23 @@ class BurndownListener < Redmine::Hook::ViewListener
     time_entry = context[:time_entry]
     issue = time_entry.issue
     return unless issue
-    debugger
-    html = <<HTML_END
+    tips = <<HTML
     <div class='flash warning'>
       Before your entry, <b>#{issue.spent_hours}</b> hours have been spent on Issue ##{issue.id} 
       (Estimated <b>#{issue.estimated_hours}</b> hours).
     </div>    
-HTML_END
+HTML
+    links = issue.children.inject('') do |buffer,child| 
+      buffer << "<li><a href='/issues/#{child.id}/time_entries/new'>##{child.id} #{child.subject}</a></li>"
+    end
+    warnning = <<HTML
+    <div class='flash error'>
+      The issue your choose for time entry is a parent one. Please entry your time into it's children:
+      <ul>
+        #{links}
+      </ul>
+    </div>    
+HTML
+    return ((issue.children.size > 0) ? warnning : tips)
   end
 end
